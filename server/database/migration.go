d10ce0f5a85a93bda01a027f0ed4da0bb286725c
Qidam Qolby <qidamqolby@gmail.com>
2023-01-04 20:40:09 +0700

    update rename file

--- server/database/migration.go
+++ server/database/migration.go
@@ -3,12 +3,12 @@ package database
 import (
 	"fmt"
 	"server/models"
-	"server/pkg/mysql"
+	"server/pkg/sql"
 )
 
 // CREATE DATABASE MYSQL
 func RunMigration() {
-	err := mysql.DB.AutoMigrate(&models.User{}, &models.Product{}, &models.Cart{}, &models.Transaction{})
+	err := sql.DB.AutoMigrate(&models.User{}, &models.Product{}, &models.Cart{}, &models.Transaction{})
 	if err != nil {
 		fmt.Println(err)
 		panic("Migration Failed")
